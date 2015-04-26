b03b821905d5387c6bb07158f553414c093352a2
ellery44 <addingtonwhiteem@beloit.edu>
2015-04-26 18:34:25 -0500

    Adds threaded class for examining what color is currently below the robot

--- src/ants/WorkerAnt.java
+++ src/ants/WorkerAnt.java
@@ -1,6 +1,9 @@
 package ants;
 
 import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
 
 public class WorkerAnt extends AntBaseClass {
 
@@ -27,11 +30,56 @@ public class WorkerAnt extends AntBaseClass {
 
 }
 
-
 class LookForColor implements Runnable {
-	public void run(){
-		//Set up color sensor
-		ColorSensor cs = new ColorSensor(SensorPort.S3);
+	private String currentColor = "None";
+
+	public void run() {
+
+		// Set up color sensor
+		EV3ColorSensor cs = new EV3ColorSensor(SensorPort.S3);
 		
+		// Start event loop
+		while (true) {
+
+			// Fetch the current color and update currentColor
+			switch (cs.getColorID()) {
+			case 0:
+				currentColor = "None";
+				break;
+			case 1:
+				currentColor = "Black";
+				break;
+			case 2:
+				currentColor = "Blue";
+				break;
+			case 3:
+				currentColor = "Green";
+				break;
+			case 4:
+				currentColor = "Yellow";
+				break;
+			case 5:
+				currentColor = "Red";
+				break;
+			case 6:
+				currentColor = "White";
+				break;
+			case 7:
+				currentColor = "Brown";
+				break;
+			}
+
+			// Take reading every half second
+			Delay.msDelay(500);
+		}
+	}
+
+	/**
+	 * Getter for the current color that the color sensor is seeing
+	 * 
+	 * @return String representation of what color is currently below the robot
+	 */
+	public String getCurrentColor() {
+		return currentColor;
 	}
 }
