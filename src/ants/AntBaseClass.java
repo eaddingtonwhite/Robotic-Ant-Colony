f5f535e2a9af605fa58d4abc79d801af2e24ed59
Jesse Rosechild <rosechildja@beloit.edu>
2015-04-28 11:02:47 -0500

    Added pilot

--- src/ants/AntBaseClass.java
+++ src/ants/AntBaseClass.java
@@ -1,11 +1,15 @@
 package ants;
 
 import java.util.UUID;
-
 import lejos.hardware.motor.Motor;
+import lejos.robotics.navigation.DifferentialPilot;
+
 
 public class AntBaseClass {
 
+	//establish a pilot
+	DifferentialPilot pilot;
+	
 	// This ants unique id
 	private UUID id;
 
@@ -13,13 +17,13 @@ public class AntBaseClass {
 	 * Creates a new ant object with a random UUID identifier
 	 */
 	public AntBaseClass() {
-
+		
+		
 		// Generates this ants id
 		this.id = UUID.randomUUID();
 
 		// Set Motor speed
-		Motor.B.setSpeed(600);
-		Motor.C.setSpeed(600);
+		pilot.setTravelSpeed(400);
 	}
 
 	/**
@@ -33,38 +37,37 @@ public class AntBaseClass {
 	/**
 	 * Moves ant forward moveAmmount
 	 */
-	public void moveForward(int moveAmmount) {
-		Motor.B.rotate(moveAmmount, true);
-		Motor.C.rotate(moveAmmount, true);
+	public void moveForward(int moveAmount) {
+		pilot.forward();
+		pilot.travel(moveAmount, true);
 	}
 
 	/**
 	 * Moves and backwards moveAmmount
 	 */
-	public void moveBackward(int moveAmmount) {
-		Motor.B.rotate(-moveAmmount, true);
-		Motor.C.rotate(-moveAmmount, true);
+	public void moveBackward(int moveAmount) {
+		pilot.backward();
+		pilot.travel(moveAmount, true);
 	}
 
 	/**
 	 * Turns ant left. Locking
 	 */
 	public void turnLeft() {
-		Motor.C.rotate(540);
+		pilot.rotate(360);
 	}
 
 	/**
 	 * Turns ant right. Locking
 	 */
 	public void turnRight() {
-		Motor.B.rotate(540);
+		pilot.rotate(-360);
 	}
 
 	/**
 	 * Stops the ant in its tracks
 	 */
 	public void stop() {
-		Motor.B.stop();
-		Motor.C.stop();
+		pilot.stop();
 	}
 }
